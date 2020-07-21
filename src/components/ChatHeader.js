fe249d77b32a4ad176301626112e6b948529db34
Tanjila Akhtar <tanjila.tisha@gmail.com>
2020-07-21 14:47:22 +0200

    Adding icon for minimize

--- src/components/ChatHeader.js
+++ src/components/ChatHeader.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 const ChatHeader = (props) => {
+  const minus = require("../images/minus-icon.png");
   return (
     <div className="chat-header" onClick={props.toggle}>
       <span>Chat</span>
-      {props.expand && <span> - </span>}
+      {props.expand && (
+        <span>
+          <img src={minus} alt="minus" width="16" />
+        </span>
+      )}
     </div>
   );
 };
 
 export default ChatHeader;
+
